a388081f223ccef1a9963e096275531c1383b7a5
Heitor Fonseca de Araujo <hfa2@windows.cin.ufpe.br>
2016-06-04 18:11:48 -0300

    atualizando

--- III/Q3.m
+++ III/Q3.m
@@ -11,12 +11,6 @@ saida110 = (im109./2+im111./2);
 saida118 = (im117./2+im119./2);
 %%}
 
-
-h = fspecial('average',[5 5])
-saida102 = imfilter(saida102,h);
-saida110 = imfilter(saida102,h);
-saida118 = imfilter(saida102,h);
-
 imwrite(saida102,'saida_102.bmp');
 imwrite(saida110,'saida_110.bmp');
 imwrite(saida118,'saida_118.bmp');
