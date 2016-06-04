90aad80d999d0715d378ac3bbc30b82bcfa5339c
Heitor Fonseca de Araujo <hfa2@windows.cin.ufpe.br>
2016-06-04 17:10:54 -0300

    adicionando questoes parte II

---
+++ II/Q2.1/Q1.m
@@ -0,0 +1,11 @@
+im = imread('C:\Users\hfa2\Desktop\New folder\Processamento_Digital_Sinais---Projeto\Arquivos\lena_rings.bmp');
+figure
+imshow(im)
+imfourier = fftshift(fft2(im));
+%imshow(log(abs(imfourier)), []); 
+figure
+h = fspecial('gaussian', [20 20], 1.1);
+y = imfilter(im,h);
+y = y*1.25;           %aumenta contraste
+y = y-10;
+imshow(y);
---
+++ II/Q2.2/Q2.m
@@ -0,0 +1,32 @@
+[im, MAP] = imread('C:\Users\hfa2\Desktop\New folder\Processamento_Digital_Sinais---Projeto\Arquivos\doc1.bmp');
+
+imContrast = im*1;
+
+level = graythresh(imContrast);
+level = 0.455;
+
+bw = im2bw(im,level);
+%bw2 = bwareaopen(bw, 15);  %Remove small objects from binary image
+bc = imcomplement(bw); % gives you the inverted version of b
+
+
+figure;
+title('Imagem Original');
+imshow(im);
+
+%{
+figure;
+title('Imagem com Contraste');
+imshow(imContrast);
+%}
+
+figure;
+title('Imagem preto e branco');
+imshow(bw);
+
+
+figure;
+title('Imagem cor invertida');
+imshow(bc);
+
+cc = bwconncomp(bc, 8)
